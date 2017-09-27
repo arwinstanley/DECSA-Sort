f625191b7cb32d2f50531b1c831c717e4f7b4503
arwinstanley <winstanleya@bishopireton.org>
2017-09-27 10:41:38 -0400

    First

--- Card Sort/src/CSMain.java
+++ Card Sort/src/CSMain.java
@@ -1,6 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class CSMain {
-
+	public static PrintWriter readerW(String fNme) {
+		File scannable = new File( fNme );
+		PrintWriter input = null;
+		try {
+			input = new PrintWriter(scannable);
+		} catch (FileNotFoundException ex) {
+			System.out.println("Cant open file: " + fNme);
+			return null;
+		}
+		return input;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
