5488f9b15e41ee809a604689ffd30b9e99435821
arwinstanley <winstanleya@bishopireton.org>
2017-09-28 12:23:55 -0400

    Second

--- Card Sort/src/Card.java
+++ Card Sort/src/Card.java
@@ -1,4 +1,29 @@
-
 public class Card {
+	private String suit;
+	private String rank;
 
+	public Card(String cardRank, String cardSuit, int cardPointValue) {
+
+		rank = cardRank;
+		suit = cardSuit;
+	}
+
+
+
+	public String suit() {
+		return suit;
+	}
+
+	
+	public String rank() {
+		return rank;
+	}
+	public boolean matches(Card otherCard) {
+		return otherCard.suit().equals(this.suit())
+			&& otherCard.rank().equals(this.rank())
+	}
+	@Override 
+	public String toString() {
+		return rank + " of " + suit;
+	}
 }
